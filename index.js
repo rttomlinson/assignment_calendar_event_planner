c4ac8e6d5bc65eb44840b078a41cb89cb659c7e4
markmarkyesyes <markhahn545@gmail.com>
2017-04-14 17:36:02 -0500

    added temp calendar view

--- index.js
+++ index.js
@@ -4,53 +4,53 @@ const bodyParser = require("body-parser");
 const morgan = require("morgan");
 /////////////**************Set up routes*************/////////////
 const usersRoutes = require("./routes/users");
+const calendarsRoutes = require("./routes/calendars");
 /////////////**************End routers*************/////////////
 const expressHandlebars = require("express-handlebars");
 const hbs = expressHandlebars.create({
-    partialsDir: "views/",
-    defaultLayout: "application"
+  partialsDir: "views/",
+  defaultLayout: "application"
 });
 
 app.use(
-    bodyParser.urlencoded({
-        extended: true
-    })
+  bodyParser.urlencoded({
+    extended: true
+  })
 );
 app.use((req, res, next) => {
-    var method;
-    if (req.query._method) {
-        method = req.query._method;
-        delete req.query._method;
-    }
-    else if (typeof req.body === "object" && req.body._method) {
-        method = req.body._method;
-        delete req.body._method;
-    }
-    if (method) {
-        method = method.toUpperCase();
-        req.method = method;
-    }
-    next();
+  var method;
+  if (req.query._method) {
+    method = req.query._method;
+    delete req.query._method;
+  } else if (typeof req.body === "object" && req.body._method) {
+    method = req.body._method;
+    delete req.body._method;
+  }
+  if (method) {
+    method = method.toUpperCase();
+    req.method = method;
+  }
+  next();
 });
 app.use(morgan("tiny"));
 app.use((req, res, next) => {
-    ["query", "params", "body"].forEach(key => {
-        if (req[key]) {
-            var capKey = key[0].toUpperCase() + key.substr(1);
-            var value = JSON.stringify(req[key], null, 2);
-            console.log(`${capKey}: ${value}`);
-        }
-    });
-    next();
+  ["query", "params", "body"].forEach(key => {
+    if (req[key]) {
+      var capKey = key[0].toUpperCase() + key.substr(1);
+      var value = JSON.stringify(req[key], null, 2);
+      console.log(`${capKey}: ${value}`);
+    }
+  });
+  next();
 });
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 app.use(express.static(__dirname + "/public"));
 
 app.use("/", usersRoutes);
-
+app.use("/calendars", calendarsRoutes);
 app.listen(process.env.PORT || 3000, () => {
-    console.log("Hey.");
+  console.log("Hey.");
 });
 
 //yarn add express express-handlebars
--- routes/calendars.js
+++ routes/calendars.js
@@ -1,3 +1,4 @@
+var express = require("express");
 var router = express.Router();
 var models = require("../models");
 var User = models.User;
@@ -5,91 +6,108 @@ let Calendar = models.Calendar;
 var sequelize = models.sequelize;
 
 var onIndex = (req, res) => {
-    User.findAll()
-        .then(users => {
-            res.render("users/index", {
-                users
-            });
-        })
-        .catch(e => res.status(500).send(e.stack));
-};
-
-let onShow = (req, res) => {
-    User.findById(req.params.id)
-        .then(user => {
-            user = user.dataValues;
-            res.render("users/showuser", {
-                user
-            });
-        })
-        .catch(e => res.status(500).send(e.stack));
-};
-
-let newUserForm = (req, res) => {
-    res.render("users/newuser");
-};
-
-let addUser = (req, res) => {
-    User.create({
-            fname: req.body.fname,
-            lname: req.body.lname,
-            username: req.body.username,
-            email: req.body.email
-        })
-        .then(function() {
-            res.redirect("users");
-        })
-        .catch(e => res.status(500).send(e.stack));
-};
-
-let onEdit = (req, res) => {
-    User.findById(req.params.id).then(user => {
-        user = user.dataValues;
-        res.render("users/edituser", {
-            user
+  var calendars;
+  Calendar.findAll({raw: true})
+    .then(cals => {
+      calendars = cals;
+      var promiseArr = [];
+      calendars.forEach(function(calendar) {
+        promiseArr.push(User.findById(calendar.userId, {raw: true}));
+      });
+      return Promise.all(promiseArr);
+    })
+    .then(users => {
+      console.log(users);
+      calendars.forEach(function(calendar) {
+        console.log(calendar);
+        users.forEach(function(user) {
+          if (calendar.userId == user.id) {
+            calendar.user = user;
+          }
         });
-    });
+      });
+
+      res.render("calendars/index", {calendars});
+    })
+    .catch(e => res.status(500).send(e.stack));
 };
 
-let editUser = (req, res) => {
-    console.log(req.body);
-    User.update({
-            fname: req.body.fname,
-            lname: req.body.lname,
-            username: req.body.username,
-            email: req.body.email
-        }, {
-            where: {
-                id: req.params.id
-            }
-        })
-        .then(function() {
-            res.redirect("/users");
-        })
-        .catch(e => res.status(500).send(e.stack));
-};
-
-let deleteUser = (req, res) => {
-    User.destroy({
-            where: {
-                id: req.params.id
-            },
-            limit: 1
-        })
-        .then(() => {
-            req.method = "GET";
-            res.redirect("/users");
-        })
-        .catch(e => res.status(500).send(e.stack));
-};
+// let onShow = (req, res) => {
+//     User.findById(req.params.id)
+//         .then(user => {
+//             user = user.dataValues;
+//             res.render("users/showuser", {
+//                 user
+//             });
+//         })
+//         .catch(e => res.status(500).send(e.stack));
+// };
+//
+// let newUserForm = (req, res) => {
+//     res.render("users/newuser");
+// };
+//
+// let addUser = (req, res) => {
+//     User.create({
+//             fname: req.body.fname,
+//             lname: req.body.lname,
+//             username: req.body.username,
+//             email: req.body.email
+//         })
+//         .then(function() {
+//             res.redirect("users");
+//         })
+//         .catch(e => res.status(500).send(e.stack));
+// };
+//
+// let onEdit = (req, res) => {
+//     User.findById(req.params.id).then(user => {
+//         user = user.dataValues;
+//         res.render("users/edituser", {
+//             user
+//         });
+//     });
+// };
+//
+// let editUser = (req, res) => {
+//     console.log(req.body);
+//     User.update({
+//             fname: req.body.fname,
+//             lname: req.body.lname,
+//             username: req.body.username,
+//             email: req.body.email
+//         }, {
+//             where: {
+//                 id: req.params.id
+//             }
+//         })
+//         .then(function() {
+//             res.redirect("/users");
+//         })
+//         .catch(e => res.status(500).send(e.stack));
+// };
+//
+// let deleteUser = (req, res) => {
+//     User.destroy({
+//             where: {
+//                 id: req.params.id
+//             },
+//             limit: 1
+//         })
+//         .then(() => {
+//             req.method = "GET";
+//             res.redirect("/users");
+//         })
+//         .catch(e => res.status(500).send(e.stack));
+// };
 
 router.get("/", onIndex);
-router.get("/users", onIndex);
-router.get("/users/new", newUserForm);
-router.get("/users/:id", onShow);
-router.get("/users/:id/edit", onEdit);
-router.delete("/users/:id/", deleteUser);
-router.post("/users/new", addUser);
-router.post("/users/:id/edit", editUser);
+// router.get("/users", onIndex);
+// router.get("/users/new", newUserForm);
+// router.get("/users/:id", onShow);
+// router.get("/users/:id/edit", onEdit);
+// router.delete("/users/:id/", deleteUser);
+// router.post("/users/new", addUser);
+// router.post("/users/:id/edit", editUser);
 
 module.exports = router;
