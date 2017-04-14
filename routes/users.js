f79897a3e01bcd606db92708c2fcd065b781afa0
markmarkyesyes <markhahn545@gmail.com>
2017-04-14 16:28:04 -0500

    added edit and delete

--- routes/users.js
+++ routes/users.js
@@ -5,61 +5,88 @@ var User = models.User;
 var sequelize = models.sequelize;
 
 var onIndex = (req, res) => {
-    User.findAll()
-        .then(users => {
-            res.render("users/index", {
-                users
-            });
-        })
-        .catch(e => res.status(500).send(e.stack));
+  User.findAll()
+    .then(users => {
+      res.render("users/index", {
+        users
+      });
+    })
+    .catch(e => res.status(500).send(e.stack));
 };
 
 let onShow = (req, res) => {
-    User.findById(req.params.id)
-        .then(user => {
-            user = user.dataValues;
-            res.render("users/showuser", {
-                user
-            });
-        })
-        .catch(e => res.status(500).send(e.stack));
+  User.findById(req.params.id)
+    .then(user => {
+      user = user.dataValues;
+      res.render("users/showuser", {
+        user
+      });
+    })
+    .catch(e => res.status(500).send(e.stack));
 };
 
 let newUserForm = (req, res) => {
-    res.render("users/newuser");
+  res.render("users/newuser");
 };
 
 let addUser = (req, res) => {
-    //Insert form data into Database
-    console.log(req.body);
-    User.create({
-            fname: req.body.fname,
-            lname: req.body.lname,
-            username: req.body.username,
-            email: req.body.email
-        }).then(function() {
-            res.redirect('/users');
-        })
-        .catch(e => res.status(500).send(e.stack));
+  User.create({
+    fname: req.body.fname,
+    lname: req.body.lname,
+    username: req.body.username,
+    email: req.body.email
+  })
+    .then(function() {
+      res.redirect("users");
+    })
+    .catch(e => res.status(500).send(e.stack));
 };
 
 let onEdit = (req, res) => {
-    User.findById(req.params.id)
-        .then(user => {
-            user = user.dataValues;
-            res.render('/users/edituser', {
-                user
-            });
-
-        });
+  User.findById(req.params.id).then(user => {
+    user = user.dataValues;
+    res.render("users/edituser", {
+      user
+    });
+  });
 };
 
+let editUser = (req, res) => {
+  console.log(req.body);
+  User.update(
+    {
+      fname: req.body.fname,
+      lname: req.body.lname,
+      username: req.body.username,
+      email: req.body.email
+    },
+    {where: {id: req.params.id}}
+  )
+    .then(function() {
+      res.redirect("/users");
+    })
+    .catch(e => res.status(500).send(e.stack));
+};
+
+let deleteUser = (req, res) => {
+  User.destroy({
+    where: {id: req.params.id},
+    limit: 1
+  })
+    .then(() => {
+      req.method = "GET";
+      res.redirect("/users");
+    })
+    .catch(e => res.status(500).send(e.stack));
+};
 
 router.get("/", onIndex);
 router.get("/users", onIndex);
 router.get("/users/new", newUserForm);
 router.get("/users/:id", onShow);
 router.get("/users/:id/edit", onEdit);
+router.delete("/users/:id/", deleteUser);
 router.post("/users/new", addUser);
+router.post("/users/:id/edit", editUser);
 
 module.exports = router;
