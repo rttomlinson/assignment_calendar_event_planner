4d93b091498597448411b6f4b412c3e57ae0f68b
Renzo Tomlinson <rttomlinson@gmail.com>
2017-04-14 17:05:36 +0000

    Add router for root path

--- index.js
+++ index.js
@@ -2,33 +2,42 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
-const index = require("./routes/index");
+
+
+
+/////////////**************Set up routes*************/////////////
+const usersRoutes = require('./routes/users');
+
+/////////////**************End routers*************/////////////
+
 const expressHandlebars = require("express-handlebars");
 const hbs = expressHandlebars.create({
-  partialsDir: "views/",
-  defaultLayout: "application"
+    partialsDir: "views/",
+    defaultLayout: "application"
 });
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(morgan("tiny"));
 app.use((req, res, next) => {
-  ["query", "params", "body"].forEach(key => {
-    if (req[key]) {
-      var capKey = key[0].toUpperCase() + key.substr(1);
-      var value = JSON.stringify(req[key], null, 2);
-      console.log(`${capKey}: ${value}`);
-    }
-  });
-  next();
+    ["query", "params", "body"].forEach(key => {
+        if (req[key]) {
+            var capKey = key[0].toUpperCase() + key.substr(1);
+            var value = JSON.stringify(req[key], null, 2);
+            console.log(`${capKey}: ${value}`);
+        }
+    });
+    next();
 });
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 app.use(express.static(__dirname + "/public"));
 
-app.use("/", index);
+app.use("/", usersRoutes);
 
-app.listen(3000, () => {
-  console.log("Hey.");
+app.listen(process.env.$PORT || 3000, () => {
+    console.log("Hey.");
 });
 
 //yarn add express express-handlebars
